3d01c232b7a4453c7a3a32e3b347e279b067ea72
Mateus Dalagnol <dalagnolm@gmail.com>
2025-07-09 22:25:20 -0300

    Departament entities class

--- application/Main.java
+++ application/Main.java
@@ -1,7 +1,12 @@
 package application;
 
+import entitites.Department;
+
 public class Main {
     public static void main(String[] args) {
         
+        Department obj = new Department(1, "Books");
+
+        System.out.println(obj);
     }
 }
